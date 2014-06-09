2c12bef9efa0116c76d50552db83dab213112903
Mark Lamley <mark.lamley@gmail.com>
2014-06-08 18:42:15 -0700

    Added status

---
+++ src/main/java/AppMain.java
@@ -0,0 +1,25 @@
+/**
+ * Created by marklamley on 2014-06-08.
+ */
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+public class AppMain {
+    public static void main(String [] args) {
+
+        System.out.println("hello world.");
+
+        // If not logged in
+
+        // Get st from GAS
+
+        // Pass st to SM. Get Cookie from SM
+
+        // Pass cookie to all SM requests.
+
+    }
+}
